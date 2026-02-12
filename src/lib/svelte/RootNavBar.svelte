2939882023f571458a49598cf113d67233def4bf
dezlymacauley <dezlymacauley@proton.me>
2026-02-12 18:57:00 +0200

    Finished adding new nav bar

--- src/lib/svelte/RootNavBar.svelte
+++ src/lib/svelte/RootNavBar.svelte
@@ -1,54 +1,66 @@
+<script lang="ts">
+  const urlTags = { target: "_blank", rel: "noopener noreferrer" };
+  const home = "https://dezlymacauley.com";
+  const youtube = "https://www.youtube.com/@dezlymacauley";
+  const github = "https://github.com/dezlymacauley?tab=overview&from=2026-01-01&to=2026-12-31";
+  const resume = "/resume/dezly_macauley_resume.pdf";
+</script>
+
+
 <style>
   @reference "$lib/css/global.css";
 
   .navbar-container {
-    /* @apply border-4 border-green-400; */
     @apply flex justify-between;
     @apply items-center;
     @apply px-4 py-2;
   }
 
   .left-section-container {
-    @apply border-4 border-pink-400;
+    /* 
+
+      TODO: Add logo later
+
+    */
+
+    @apply flex gap-2;
   }
   
   .right-section-container {
-
+    @apply flex gap-4;
+    @apply items-center;
   }
 
-  .dezlymacauley-dot-com-link {
-    @apply text-gray-300 text-4xl font-semibold;
+  .home-link {
+    @apply text-gray-300 text-3xl font-semibold;
     @apply inline-block;
-    @apply cursor-pointer;
   }
 
   .right-section-link {
-    /* @apply px-4; */
-    /* @apply inline-block; */
-    /* @apply px-5 py-2; */
-    /* @apply text-gray-300 text-lg font-semibold; */
-    /* @apply cursor-pointer; */
+    @apply bg-gray-500 px-5 py-2 rounded-3xl;
+    @apply inline-block;
+    @apply text-gray-300 text-lg font-semibold;
   }
 
   .youtube-link {
-    /* @apply bg-red-700; */
-    /* @apply rounded-3xl; */
-    /* @apply shadow-lg shadow-red-700/75; */
-    /* @apply hover:shadow-xl hover:text-white hover:font-bold hover:bg-red-600; */
+    @apply bg-red-700;
+    @apply shadow-lg shadow-red-700/75;
+    @apply hover:shadow-xl hover:text-white hover:font-bold hover:bg-red-600;
+    @apply transition duration-300;
   }
 
   .github-link {
-    /* @apply bg-blue-700; */
-    /* @apply rounded-3xl; */
-    /* @apply shadow-lg shadow-blue-700/75; */
-    /* @apply hover:shadow-xl hover:text-white hover:font-bold hover:bg-blue-600; */
+    @apply bg-blue-700;
+    @apply shadow-lg shadow-blue-700/75;
+    @apply hover:shadow-xl hover:text-white hover:font-bold hover:bg-blue-600;
+    @apply transition duration-300;
   }
 
   .resume-link {
-    /* @apply bg-[#610079]; */
-    /* @apply rounded-3xl; */
-    /* @apply shadow-lg shadow-[#610079]/75; */
-    /* @apply hover:shadow-xl hover:text-white hover:font-bold hover:bg-[#9f00c6]; */
+    @apply bg-[#610079];
+    @apply shadow-lg shadow-[#610079]/75;
+    @apply hover:shadow-xl hover:text-white hover:font-bold hover:bg-[#9f00c6];
+    @apply transition duration-300;
   }
 
 </style>
@@ -58,40 +70,32 @@
 <nav class="navbar-container">
 
   <div class="left-section-container">
-
     <h1>
-      <a
-        class="dezlymacauley-dot-com-link"
-        href="https://dezlymacauley.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >Dezly Macauley</a>
+      <a class="home-link" href={home} {...urlTags}>Dezly Macauley</a>
     </h1>
-
   </div>
 
   <!-- Right Section -->
-  <div class="right-section-link border-4 border-orange-400 flex">
+  <div class="right-section-container">
+
     <a
-      href="https://www.youtube.com/@dezlymacauley"
-      class="youtube-link"
-      target="_blank"
-      rel="noopener noreferrer"
+      href={youtube}
+      class="right-section-link youtube-link"
+      {...urlTags}
     >
       YouTube
     </a>
 
     <a
-      href="https://github.com/dezlymacauley?tab=overview&from=2026-01-01&to=2026-12-31"
+      href={github}
       class="right-section-link github-link"
-      target="_blank"
-      rel="noopener noreferrer"
+      {...urlTags}
     >
       GitHub
     </a>
 
     <a
-      href="/resume/dezly_macauley_resume.pdf"
+      href={resume}
       class="right-section-link resume-link"
       download
     >
