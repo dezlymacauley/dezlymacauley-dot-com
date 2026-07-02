cde9a365bae92015cc031d2a9d359d48fe61c53d
dezlymacauley <dezlymacauley@proton.me>
2026-07-02 15:32:45 +0200

    Added a GitHub link to the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,27 @@
+<script lang="ts">
+  const resume: string = "dezly_macauley_resume.pdf";
+  const github: string = "https://github.com/dezlymacauley";
+  const target: string = "_blank";
+  const rel: string = "noreferrer";
+</script>
+
+<!---------------------------------------------------------------------------->
+
 <h1>Dezly Macauley</h1>
 
 <div>
-  <a href="dezly_macauley_resume.pdf" target="_blank">View Resume</a>
+  <a href={github} {target} {rel}>GitHub</a>
+  <a href={resume} {target}>Resume</a>
 </div>
 
 <!---------------------------------------------------------------------------->
 
 <style>
   div {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    
     margin-top: 10px;
   }
 </style>
