aca6da51eab9dcb75a23c3d356d3ca3def89e45c
dezlymacauley <dezlymacauley@proton.me>
2026-07-02 13:53:15 +0200

    Added .odt and pdf resume. Added resume link to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
 <h1>Dezly Macauley</h1>
+
+<div>
+  <a href="dezly_macauley_resume.pdf" target="_blank">View Resume</a>
+</div>
+
+<!---------------------------------------------------------------------------->
+
+<style>
+  div {
+    margin-top: 10px;
+  }
+</style>
