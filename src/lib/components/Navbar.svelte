bc879d948610e105620b59fee876dc5eea6e9dcc
dezlymacauley <dezlymacauley@proton.me>
2026-07-16 08:54:25 +0200

    added links for basic Navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,30 @@
+<!---------------------------------------------------------------------------->
+
+<!--
+  HTML:
+-->
+
+<nav class="navbar-container border-4 border-red-500">
+  <a href="https://github.com/dezlymacauley" target="_blank"
+    rel="noopener noreferrer" class="nav-link border-4 border-purple-500">
+  GitHub
+  </a>
+  <a href="https://youtube.com/@dezlymacauley" target="_blank"
+    rel="noopener noreferrer" class="nav-link border-4 border-purple-500">
+  YouTube
+  </a>
+  <a href="/dezly_macauley_resume.pdf" target="_blank"
+    class="nav-link border-4 border-purple-500">Resume</a>
+</nav>
+
+<!---------------------------------------------------------------------------->
+
+<!--
+  TAILWIND_CSS:
+-->
+
+<style>
+  @reference "$lib/global.css";
+</style>
+
+<!---------------------------------------------------------------------------->
